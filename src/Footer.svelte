69ac8c1db42b6d3fdc31ec876ddfef1a7736ba7e
* air <134613063+Jakob-CC@users.noreply.github.com>
2023-08-29 22:15:06 +0200

    Updates

--- src/Footer.svelte
+++
@@ -1,8 +0,0 @@
-<footer class="uk-text-center uk-text-small uk-padding-small uk-margin-top-xlarge uk-background-secondary">
-    <a class="uk-link-muted"   href="https://eusuhm.org" target='_blank' rel='noopener'>
-        
-        EUSUHM &emsp; European Union for School and University Health and Medicine
-        
-    </a>
-    <!-- imprint -->
-</footer>
