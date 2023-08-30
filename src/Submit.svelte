cd8225422ddbf90b3b1d0e1d9857d10a0537790a
Jakob M <j.malek@congress-compact.de>
2023-08-30 17:11:17 +0200

    topics improved. button improved

--- src/Submit.svelte
+++ src/Submit.svelte
@@ -1,25 +1,25 @@
 <script>
     let hottopics = [
-        "Samples of topics",
         "Sustainable Health in Children and Students – Reduce the Gap!",
         "Bridging Health and Education Gaps: Lessons Learnt!",
         "Digital World and Impact on Children’s and Adolescents’ Wellbeing",
-        "10 Interactive Rooms* - Take Away Message!",
-        
         "Data for Action: Monitoring Health in Children, Adolescents and Students",
         "Intersectoral Interventions: Public Health on Site",
+        
         "School Nurses and Networking",
         "Improving HPV Vaccination in Europe - Lessons Learnt",
         "Vision and Hearing Screening",
+        "Gender Dysphoria",
+        "Inclusion of Children with Chronic Health Conditions: Models of Good Practice",
         
         
         ];
 </script>
 
 <!-- Submit Component -->
-<div class="uk-container">
-    <h1 class="uk-heading-large uk-padding-large uk-text-center">Submit your abstract here</h1>
-    <button class="uk-button uk-button-primary uk-button-large">Submit</button> 
+<div class="uk-container uk-padding-large">
+    <h1 class="uk-heading-large uk-text-center">Submit your abstract here</h1>
+    <button class="uk-button uk-button-primary uk-button-large uk-margin">Submit</button> 
     <h2>Our main topics in 2024</h2>
     <ul class="uk-list uk-list-large">
         {#each hottopics as hottopic}
