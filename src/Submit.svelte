173da64a91266d420f6c3ff919df7f4c864dee0b
Jakob M <j.malek@congress-compact.de>
2023-08-30 16:44:43 +0200

    basic submit subpage.

--- src/Submit.svelte
+++ src/Submit.svelte
@@ -1,2 +1,30 @@
+<script>
+    let hottopics = [
+        "Samples of topics",
+        "Sustainable Health in Children and Students – Reduce the Gap!",
+        "Bridging Health and Education Gaps: Lessons Learnt!",
+        "Digital World and Impact on Children’s and Adolescents’ Wellbeing",
+        "10 Interactive Rooms* - Take Away Message!",
+        
+        "Data for Action: Monitoring Health in Children, Adolescents and Students",
+        "Intersectoral Interventions: Public Health on Site",
+        "School Nurses and Networking",
+        "Improving HPV Vaccination in Europe - Lessons Learnt",
+        "Vision and Hearing Screening",
+        
+        
+        ];
+</script>
+
 <!-- Submit Component -->
-<div class="submit-container uk-card uk-card">Hello Submit</div>
+<div class="uk-container">
+    <h1 class="uk-heading-large">Submit your abstract here</h1>
+    <h2>Our main topics in 2024</h2>
+
+    <ul class="uk-list uk-list-large">
+        {#each hottopics as hottopic}
+            <li>{hottopic}</li>
+        {/each}
+    </ul>
+
+</div> 
