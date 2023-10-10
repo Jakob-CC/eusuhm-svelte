07f1b22941e44ec04e9bce1a17a60455015dbdcb
Jakob M <j.malek@congress-compact.de>
2023-10-10 16:39:48 +0200

    Landing page with bvögd infos skelleton ready

--- src/CongressData.js
+++ src/CongressData.js
@@ -1,11 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const congressdata = writable({
-    'Programme Committee': {
-      'BVÖGD': ['Gabriele Ellsäßer', 'Bettina Langenbruch', 'Karlin Stark', 'Gabriele Trost-Brinkhues', 'Andrea Wünsch', 'Claudia Korebrits'],
-      'ScolarMed': ['Tina Huber-Gieseke', 'Susanne Stronski'],
-      'EUSUHM\'s Executive Committee': ['Noora Seilo', 'Marija Posavec', 'Bernarda Vogrin', 'Zophia Nagy', 'Lineke Dogger']
-    },
     'Wissenschaftliche Leitung':
     {   
         name: 'TBD',
@@ -15,11 +10,24 @@ export const congressdata = writable({
     {
         content:'Certification of the event will be applied for at the Hamburg Medical Association "Ärztekammer Hamburg".'
     },
-    'Organisation und Anmeldung':
+    'Programme Committee': {
+      'BVÖGD': ['Gabriele Ellsäßer', 'Bettina Langenbruch', 'Karlin Stark', 'Gabriele Trost-Brinkhues', 'Andrea Wünsch', 'Claudia Korebrits'],
+      'ScolarMed': ['Tina Huber-Gieseke', 'Susanne Stronski'],
+      'EUSUHM\'s Executive Committee': ['Noora Seilo', 'Marija Posavec', 'Bernarda Vogrin', 'Zophia Nagy', 'Lineke Dogger']
+    },
+    'Location': {
+        name: 'Oberlinschule',
+        address: 'Rudolf-Breitscheid-Straße 24',
+        city: '14482 Potsdam',
+      },
+    'Organisation':
         {
             company: 'Congress Compact 2C GmbH Congress Compact 2C GmbH',
             contact: 'Gina Braun',
             address: 'Joachimsthaler Straße 31-32, 10719 Berlin',
+            phone: '+49 30 88727370',
+            fax: '+49 30 887273710',
+            email: 'info@congress-compact.de',
         },
-       
+    
   });
