efd4d8d8c3921d8a6f32a3f6073b60f3cf3d6e98
Jakob M <j.malek@congress-compact.de>
2023-10-10 15:08:04 +0200

    removed contact form

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -16,38 +16,6 @@
                     <p class="uk-margin-remove">Email <a class="uk-link-muted" href="mailto:info@congress-compact.de">info@congress-compact.de</a></p>
                 </address>    
             </div>
-
-
-
-
-
-            <!-- Contact Form -->
-            <!-- <div class="uk-padding-large uk-background-muted uk-border-rounded">
-                <form class="uk-form-stacked">
-                    <div class="">
-                        <label class="uk-form-label uk-text-muted " for="contact-name">Name</label>
-                        <div class="uk-form-controls">
-                            <input class="uk-input uk-border-rounded" id="contact-name" type="text" placeholder="Your Name">
-                        </div>
-                    </div>
-                    <div class="uk-margin-small">
-                        <label class="uk-form-label uk-text-muted " for="contact-email">Email</label>
-                        <div class="uk-form-controls">
-                            <input class="uk-input uk-border-rounded" id="contact-email" type="email" placeholder="yourname@email.com">
-                        </div>
-                    </div>
-                    <div class="uk-margin-small">
-                        <label class="uk-form-label uk-text-muted  " for="contact-message">Message</label>
-                        <div class="uk-form-controls">
-                            <textarea class="uk-textarea uk-border-rounded" id="contact-message" rows="5" placeholder="Your Message"></textarea>
-                        </div>
-                    </div>
-                    <div class="uk-margin-small-top">
-                        <button type="submit" class="uk-button uk-button-muted uk-button-large uk-width-1-1">Send Message</button>
-                    </div>
-                </form>
-            </div>
-             -->
         </div>
     </article>
 </section>
