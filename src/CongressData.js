89ad141036dc1510ec30820ccaad7aaad3e740c3
Jakob M <j.malek@congress-compact.de>
2023-10-10 17:15:40 +0200

    first formatting started

--- src/CongressData.js
+++ src/CongressData.js
@@ -1,33 +1,38 @@
 import { writable } from 'svelte/store';
 
 export const congressdata = writable({
-    'Wissenschaftliche Leitung':
-    {   
+    'Wissenschaftliche Leitung': {   
         name: 'TBD',
         organisation: 'TBD'
     },
-    'Certification':
-    {
+    'Certification': {
         content:'Certification of the event will be applied for at the Hamburg Medical Association "Ärztekammer Hamburg".'
     },
-    'Programme Committee': {
-      'BVÖGD': ['Gabriele Ellsäßer', 'Bettina Langenbruch', 'Karlin Stark', 'Gabriele Trost-Brinkhues', 'Andrea Wünsch', 'Claudia Korebrits'],
-      'ScolarMed': ['Tina Huber-Gieseke', 'Susanne Stronski'],
-      'EUSUHM\'s Executive Committee': ['Noora Seilo', 'Marija Posavec', 'Bernarda Vogrin', 'Zophia Nagy', 'Lineke Dogger']
-    },
+    'Programme Committee': [
+        {
+            organisation: 'BVÖGD',
+            members: ['Gabriele Ellsäßer', 'Bettina Langenbruch', 'Karlin Stark', 'Gabriele Trost-Brinkhues', 'Andrea Wünsch', 'Claudia Korebrits']
+        },
+        {
+            organisation: 'ScolarMed',
+            members: ['Tina Huber-Gieseke', 'Susanne Stronski']
+        },
+        {
+            organisation: 'EUSUHM\'s Executive Committee',
+            members: ['Noora Seilo', 'Marija Posavec', 'Bernarda Vogrin', 'Zophia Nagy', 'Lineke Dogger']
+        }
+    ],
     'Location': {
         name: 'Oberlinschule',
         address: 'Rudolf-Breitscheid-Straße 24',
         city: '14482 Potsdam',
-      },
-    'Organisation':
-        {
-            company: 'Congress Compact 2C GmbH Congress Compact 2C GmbH',
-            contact: 'Gina Braun',
-            address: 'Joachimsthaler Straße 31-32, 10719 Berlin',
-            phone: '+49 30 88727370',
-            fax: '+49 30 887273710',
-            email: 'info@congress-compact.de',
-        },
-    
-  });
+    },
+    'Organisation': {
+        company: 'Congress Compact 2C GmbH Congress Compact 2C GmbH',
+        contact: 'Gina Braun',
+        address: 'Joachimsthaler Straße 31-32, 10719 Berlin',
+        phone: '+49 30 88727370',
+        fax: '+49 30 887273710',
+        email: 'info@congress-compact.de',
+    },    
+});
