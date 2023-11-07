191348bd2411253a3b7706faafc041e45b9f0d64
Jakob M <j.malek@congress-compact.de>
2023-11-07 19:27:40 +0100

    file structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import App from './App.svelte';
-// import eusuhmLogo from './assets/img/logo-separat/eusuhm_logo.svg';
 
-// uikit
+// Javascript wird hier eingebunden
 import './assets/uikit.min.js';
 // import './assets/uikit-icons.min.js';
 
