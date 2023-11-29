34eedd627573da6bcc592f01cb4235b064e70d67
Jakob M <j.malek@congress-compact.de>
2023-11-29 15:32:33 +0100

    Subission page major update

--- src/Submit.svelte
+++ src/Submit.svelte
@@ -13,4 +13,87 @@
             </button> 
         </a>        
     </div>
+    <h2 class="uk-heading eus-normcase">Rules for Abstract Submission</h2>
+    <p>Please read the submission rules before submitting an abstract.</p>
+    <h3 class="eus-normcase">Abstracts must be submitted online via the website only (www.EUSUHM.info).</h3>
+    <h4 class="eus-normcase">Submission and Presentation Type</h4>
+    <ul>
+        <li>Abstracts may be submitted for oral or printed poster presentation. The Programme Committee will determine whether the abstract will be accepted for presentation, with consideration given to the author’s preference.</li>
+        <li>Abstracts must be received by the announced deadline. Abstracts received after the deadline will not be considered.</li>
+    </ul>
+    <h4 class="eus-normcase">Limitations</h4>
+    <ul>
+        <li><span class="eus-highlight">Presenting author must be registered participant.</span> Only abstracts of authors who have paid their registration fees by <span class="eus-highlight">the Early Registration Deadline</span> will be scheduled for presentation and included for publication.</li>
+        <li>The presenting author is required to ensure that all co-authors are aware of the content of the abstract and agree to its submission, before submitting the abstract.</li>
+        <li>Abstracts must be submitted in British English.</li>
+        <li>Disclosure of Conflicts of Interest: Abstract submitters will be required to disclose any conflict of interests in the submission form.</li>
+    </ul>
+
+    <h3 class="eus-normcase">Guidelines for Submission</h3>
+    <h4 class="uk-margin-remove eus-normcase">Before you begin, please prepare the following information.</h4>
+    <ul>
+        
+        <li class="eus-highlight">Presenting author’s contact details.</li>
+        <li>Full first and family name(s)</li>
+        <li>Email address</li>
+        <li>Affiliation details: department, institution, city, state (if relevant), country</li>
+        <li>Phone number.</li>
+        <li>Author’s and co-authors’ details</li>
+        
+    </ul>
+    <h4 class="eus-normcase">Preferred Presentation type</h4>
+    <p>     Oral or E-Poster </p>
+    <h3 class="eus-normcase">Abstract Content</h3>
+    <ul>
+        <li><span class="eus-highlight">Abstract title</span> – must be in <span class="eus-highlight">UPPER CASE</span> and <span class="eus-highlight">limited to 150 characters</span>.</li>
+        <li><span class="eus-highlight">Abstract text</span> – is limited to <span class="eus-highlight">3000 characters</span> including acknowledgements.</li>
+        <li><span class="eus-highlight">Abstract topic</span> – select the abstract topic from the <a href="/programme" target="_blank" rel="noopener">list of topics.</a>
+            <ul>
+                <li>Please note, that the list of topics refers to settings for children, adolescents, and students</li>
+                <li>
+                    Abstracts should clearly state:
+                    <ul>
+                        <li>Background and aims</li>
+                        <li>Methods</li>
+                        <li>Results</li>
+                        <li>Conclusions with Take Home Messages</li>
+                    </ul></li>
+            </ul></li>
+        <li><span class="eus-highlight">Abstract with case report</span>
+            <ul>
+                <li>Case Descriptions Including Health and Education Needs  </li>
+                <li>Intervention</li>
+                <li>Take Home Message</li>
+            </ul>
+
+        </li>
+    </ul>
+    <h3 class="eus-normcase">Completion of Procedure</h3>
+    <p>
+        You will receive an abstract ID number via email after you have submitted your abstract.
+        Please refer to this abstract number in all correspondence regarding the abstract.
+        Please <a href="mailto:info@eusuhm.info">contact us</a> if you have not received confirmation that your abstract has been submitted.
+    </p>
+
+
+    <h3 class="eus-normcase">List of Topics Referring to Settings for Children, Adolescents, Students </h3>
+    <ul>
+        <li>Early Childhood Interventions</li>
+        <li>Showcasing School Health Services </li>
+        <li>Mental Health </li>
+        <li>Chronic Health Conditions, Including Case Reports</li>
+        <li>Special Needs</li>
+        <li>School Absences & School Health Services</li>
+        <li>Qualification of Health Professionals</li>
+        <li>Health Monitoring – Data for Action</li>
+        <li>Health Promotion & School Health Services </li>
+        <li>Sexuality, Puberty, Diversity</li>
+        <li>Post Pandemic - Impact on Health </li>
+        <li>Intersectoral Collaboration</li>
+        <li>Transition   </li>
+        <li>Free Topics</li>
+    </ul>
+
+
+
 </div> 
